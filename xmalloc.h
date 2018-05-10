9b74e6389c116add5f41617d2644763c89d02524
zhu <ziqiang_free@163.com>
2018-05-10 17:58:21 +0800

    add xmalloc implementation, more check than malloc, and exit process if malloc failed.

---
+++ xmalloc.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: xmalloc.h
+	> Author: ziqiang
+	> Mail: ziqiang_free@163.com 
+	> Created Time: Thu 10 May 2018 10:33:18 AM CST
+ ************************************************************************/
+
+#ifndef XMALLOC_H_
+#define XMALLOC_H_
+
+void * xmalloc(size_t size);
+
+#endif
